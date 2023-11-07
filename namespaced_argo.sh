e6430e5a8348552f925981a577df8a3e607ddc66
YAHIA AHMED <33053353+yoyoraso@users.noreply.github.com>
2023-11-07 20:46:38 +0200

    Update namespaced_argo.sh

--- namespaced_argo.sh
+++ namespaced_argo.sh
@@ -1,79 +1,33 @@
 #!/bin/bash
-mkdir -pv /tmp/argo-cd
-git clone https://github.com/argoproj/argo-cd.git /tmp/argo-cd
+# Install PreRequisites
+source ./utils/prerequisites.sh
 
-# Cluster Admin must install CRDs first 
-# Install appproject CRD
-kubectl apply --filename=https://github.com/argoproj/argo-cd/blob/master/manifests/crds/appproject-crd.yaml?raw=true
 
-# Install application CRD
-kubectl apply --filename=https://github.com/argoproj/argo-cd/blob/master/manifests/crds/application-crd.yaml?raw=true
+if whiptail  --msgbox "Hi, Welcome to ArgoCD install program!\n\nPress OK to proceed or Esc to exit." 10 80 ; then
 
-# Install AppSet CRD
-kubectl apply --filename=https://github.com/argoproj/argo-cd/blob/master/manifests/crds/applicationset-crd.yaml?raw=true
+ export M_NS=$(whiptail --inputbox "Enter the Namespace where ArgoCD will be  installed" 10 40 3>&1 1>&2 2>&3)
 
-# Main namespace is where argocd will be deployed
-export main_namespace=argocd
-export target_namespaces=dev,prod
+ export T_NS=$(whiptail --inputbox "Enter the Namespaces which ArgoCD will manage separated by commas" 10 40 3>&1 1>&2 2>&3)
 
-## Main Namespace setup ##
+ echo "export main_namespace=$M_NS" > ./file.sh
+ echo "export target_namespaces=$T_NS" >> ./file.sh
 
-# Create argocd manager service account in main namespace 
-kubectl create serviceaccount argocd-manager --namespace=$main_namespace
+ COUNTER=0
+ bash ./argocd.sh  >/dev/null 2>&1 &
+ 
+ 
+ while [ -n "$(ps aux | grep -v 'grep' | grep argocd.sh)" ]; do
+  sleep 4.8
+  COUNTER=$(($COUNTER+10))
+  if [[ -z "$(ps aux | grep -v 'grep' | grep argocd.sh)" ]];then
+        COUNTER=100
+        echo ${COUNTER}
+        break;
+  fi
+  echo ${COUNTER}
+ done | whiptail --gauge 'Installing ArgoCD...' 6 60 0
 
-# Create argocd manager service account Token in main namespace
-kubectl apply --filename argocd-manager-secret.yaml --namespace=$main_namespace
-
-# Create argocd manager role in main namespace
-kubectl apply --filename argocd-manager-role.yaml --namespace=$main_namespace
-
-# Create argocd manager role binding in main namespace
-kubectl create rolebinding argocd-manager-role-binding \
---namespace $main_namespace \
---role argocd-manager-role \
---serviceaccount=$main_namespace:argocd-manager
-
-# Grab the token from the secret
-export argocd_manager_token=$( kubectl get secret argocd-manager --output=json --namespace=argocd | jq --raw-output .data.token | base64 --decode)
-
-# Create argocd-managed secret
-kubectl delete secret cluster-kubernetes.default.svc-argocd-managed --ignore-not-found --namespace=$main_namespace
-
-kubectl create secret generic cluster-kubernetes.default.svc-argocd-managed \
---namespace=$main_namespace \
---from-literal=config="{\"bearerToken\":\"$argocd_manager_token\",\"tlsClientConfig\":{\"insecure\":true}}" \
---from-literal=name=argocd-managed \
---from-literal=namespaces=$target_namespaces \
---from-literal=server=https://kubernetes.default.svc
-
-# Label cluster-secret
-kubectl label secret cluster-kubernetes.default.svc-argocd-managed \
---namespace=$main_namespace \
---overwrite \
-argocd.argoproj.io/secret-type=cluster
-
-# Annotate cluster-secret
-kubectl annotate secret cluster-kubernetes.default.svc-argocd-managed \
---namespace=$main_namespace \
---overwrite \
-managed-by=argocd.argoproj.io
-
-## Target Namespace setup ##
-# Create argocd manager role and rolebinding on all target namespaces
-# https://stackoverflow.com/a/35894538
-for namespace in ${target_namespaces//,/ }
-do
-  kubectl apply --namespace $namespace \
-  --filename argocd-manager-role.yaml
-
-  kubectl create rolebinding argocd-manager-role-binding \
-  --namespace $namespace \
-  --role argocd-manager-role \
-  --serviceaccount=$main_namespace:argocd-manager
-done
-
-# Create required service accounts, services and roles
-kustomize build /tmp/argo-cd/manifests/base | kubectl apply --namespace=$main_namespace --filename -  
-
-# Replace argocd-cm 
-kubectl apply --filename argocd-cm.yaml --namespace=$main_namespace
+else
+  TERM=ansi
+  whiptail --infobox "Cancelling the process" 10 40
+fi
